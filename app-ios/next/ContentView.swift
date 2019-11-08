c1226aa936359204007994f6fb3c1fb91950d2d6
Hide <itomakiweb@users.noreply.github.com>
2019-11-08 19:34:11 +0900

    init app-ios

---
+++ app-ios/next/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  next
+//
+//  Created by 細田英幸 on 2019/11/08.
+//  Copyright © 2019 itomakiweb-corp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
